1e69c7d8a91c3bbd915f81ce9ce7b84d9e3570fa
Jacob-MUN <77593128+Jacob-MUN@users.noreply.github.com>
2021-03-07 15:13:53 -0330

    Stats Listed

---
+++ src/index.js
@@ -0,0 +1,21 @@
+let health = 5135;
+let autoMin = 0;
+let autoMax = 0;
+let strength = 405;
+let physDef = 0;
+let ether = 417;
+let etherDef = 0;
+let agility = 117;
+let crit = 5;
+let block = 0;
+
+document.getElementById("health").innerHTML = health;
+document.getElementById("autoMin").innerHTML = autoMin;
+document.getElementById("autoMax").innerHTML = autoMax;
+document.getElementById("strength").innerHTML = strength;
+document.getElementById("physDef").innerHTML = physDef;
+document.getElementById("ether").innerHTML = ether;
+document.getElementById("etherDef").innerHTML = etherDef;
+document.getElementById("agility").innerHTML = agility;
+document.getElementById("crit").innerHTML = crit;
+document.getElementById("block").innerHTML = block;
