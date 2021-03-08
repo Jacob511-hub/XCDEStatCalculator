0b30ed18b45bec29ac1891d0e6c55b2ec8dc33ea
Jacob-MUN <77593128+Jacob-MUN@users.noreply.github.com>
2021-03-08 20:28:55 -0330

    Shulk Weapon list functioning

--- src/index.js
+++ src/index.js
@@ -16,15 +16,114 @@ let weaponEtherDef = 0;
 let weaponCrit = 0;
 let weaponBlock = 0;
 
+function weaponSwitch() {
+
+    switch (document.getElementById('weapons').value)
+    {
+        case 'none':
+            weaponMin = 0;
+            weaponMax = 0;
+            weaponPhysDef = 0;
+            weaponEtherDef = 0;
+            weaponCrit = 0;
+            weaponBlock = 0;
+            statCalc();
+            break;
+        case 'monado':
+            weaponMin = 999;
+            weaponMax = 999;
+            weaponPhysDef = 0;
+            weaponEtherDef = 0;
+            weaponCrit = 10;
+            weaponBlock = 0;
+            statCalc();
+            break;
+        case 'monadoII':
+            weaponMin = 999;
+            weaponMax = 999;
+            weaponPhysDef = 0;
+            weaponEtherDef = 0;
+            weaponCrit = 10;
+            weaponBlock = 0;
+            statCalc();
+            break;
+        case 'replicaMonado':
+            weaponMin = 831;
+            weaponMax = 999;
+            weaponPhysDef = 25;
+            weaponEtherDef = 25;
+            weaponCrit = 10;
+            weaponBlock = 5;
+            statCalc();
+            break;
+        case 'rudraMonado':
+            weaponMin = 970;
+            weaponMax = 999;
+            weaponPhysDef = 25;
+            weaponEtherDef = 25;
+            weaponCrit = 5;
+            weaponBlock = 5;
+            statCalc();
+            break;
+        case 'agniMonado':
+            weaponMin = 554;
+            weaponMax = 999;
+            weaponPhysDef = 50;
+            weaponEtherDef = 50;
+            weaponCrit = 10;
+            weaponBlock = 25;
+            statCalc();
+            break;
+        case 'abyssMonado':
+            weaponMin = 831;
+            weaponMax = 999;
+            weaponPhysDef = 25;
+            weaponEtherDef = 25;
+            weaponCrit = 15;
+            weaponBlock = 5;
+            statCalc();
+            break;
+        case 'dogmaMonado':
+            weaponMin = 831;
+            weaponMax = 999;
+            weaponPhysDef = 0;
+            weaponEtherDef = 0;
+            weaponCrit = 25;
+            weaponBlock = 0;
+            statCalc();
+            break;
+        case 'sagaMonado':
+            weaponMin = 693;
+            weaponMax = 891;
+            weaponPhysDef = 150;
+            weaponEtherDef = 150;
+            weaponCrit = 5;
+            weaponBlock = 15;
+            statCalc();
+            break;
+        case 'monadoIII':
+            weaponMin = 999;
+            weaponMax = 999;
+            weaponPhysDef = 25;
+            weaponEtherDef = 25;
+            weaponCrit = 75;
+            weaponBlock = 15;
+            statCalc();
+            break;
+    }
+}
+
+function statCalc() {
+
 let totalAutoMin = autoMin + weaponMin + strength;
 let totalAutoMax = autoMax + weaponMax + strength;
 let totalStrength = strength;
-let totalPhysDef = physDef;
+let totalPhysDef = physDef + weaponPhysDef;
 let totalEther = ether;
-let totalEtherDef = etherDef;
+let totalEtherDef = etherDef + weaponEtherDef;
 let totalAgility = agility;
-let totalCrit = crit;
-let totalBlock = block;
+let totalCrit = crit + weaponCrit;
+let totalBlock = block + weaponBlock;
 
 document.getElementById("health").innerHTML = "HP: " + health;
 document.getElementById("autoMin").innerHTML = "Auto-Attack Min: " + totalAutoMin;
@@ -34,31 +133,7 @@ document.getElementById("physDef").innerHTML = "Physical Def: " + totalPhysDef;
 document.getElementById("ether").innerHTML = "Ether: " + totalEther;
 document.getElementById("etherDef").innerHTML = "Ether Def: " + totalEtherDef;
 document.getElementById("agility").innerHTML = "Agility: " + totalAgility;
-document.getElementById("crit").innerHTML = "Critical Rate: " + totalCrit;
-document.getElementById("block").innerHTML = "Block Rate: " + totalBlock;
+document.getElementById("crit").innerHTML = "Critical Rate: " + totalCrit + "%";
+document.getElementById("block").innerHTML = "Block Rate: " + totalBlock + "%";
 
-/* const update = document.getElementById("updateStats");
-update.addEventListener("click", () => { */
-
-let weapon = document.getElementById("weapons").value;
-
-switch (weapon)
-{
-    case "0":
-        weaponMin = 0;
-        weaponMax = 0;
-        weaponPhysDef = 0;
-        weaponEtherDef = 0;
-        weaponCrit = 0;
-        weaponBlock = 0;
-        break;
-    case "1":
-        weaponMin = 999;
-        weaponMax = 999;
-        weaponPhysDef = 0;
-        weaponEtherDef = 0;
-        weaponCrit = 10;
-        weaponBlock = 0;
-        break;
 }
-/* }); */
